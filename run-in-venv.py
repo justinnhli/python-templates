61e661ecf468296e8d2fb01c06655539c0591f48
Justin Li <justinnhli@gmail.com>
2020-09-23 17:51:17 -0700

    update run-in-venv

--- run-in-venv.py
+++ run-in-venv.py
@@ -1,12 +1,17 @@
 try:
     pass # FIXME
-except ModuleNotFoundError as err:
+except (ModuleNotFoundError, ImportError) as err:
 
     def run_with_venv(venv):
+        # type: (str) -> None
         """Run this script in a virtual environment.
 
         Parameters:
             venv (str): The virtual environment to use.
+
+        Raises:
+            FileNotFoundError: If the virtual environment does not exist.
+            ImportError: If the virtual environment does not contain the necessary packages.
         """
         # pylint: disable = ungrouped-imports, reimported, redefined-outer-name, import-outside-toplevel
         import sys
@@ -14,7 +19,7 @@ except ModuleNotFoundError as err:
         from pathlib import Path
         venv_python = Path(environ['PYTHON_VENV_HOME'], venv, 'bin', 'python3').expanduser()
         if not venv_python.exists():
-            raise FileNotFoundError(f'could not find venv "{venv}" for executable {venv_python}')
+            raise FileNotFoundError(f'could not find venv "{venv}" at executable {venv_python}')
         if sys.executable == str(venv_python):
             raise ImportError(f'no module {err.name} in venv "{venv}" ({venv_python})')
         execv(str(venv_python), [str(venv_python), *sys.argv])
